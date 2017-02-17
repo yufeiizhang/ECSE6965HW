46deea3704c3a8716c84cca789cdc703a0ccee22
Yufeiizhang <yufeiizhang@foxmail.com>
2017-02-16 20:05:21 -0500

    20170216 afternoon

--- HW02/HW01.py
+++ HW02/HW01.py
@@ -3,32 +3,34 @@
 # pylint: disable=I0011,C0103
 
 '''
-Just want to load image via python3.5
+Program Assigment 2
+1st question
 '''
 
 # import image module
 from PIL import Image
 
-# store Image
-side = 28
-img = [[0]*side for i in range(side)]
-
-# url
+# prepare urls
 gUrl = "C:\\Users\\Yufei\\Desktop\\deep learning\\"
 lUrl = "program assignment 2\\data_prog2\\"
 sTest = "test_data\\"
 sTrain = "train_data\\"
-
-# image number
-nImage = 0
 suffix = ".jpg"
-for im in range(10):
+
+# load data from training dataset
+nImgTrain = 25112
+# nImgTrain = 4
+imgTrain = []
+for im in range(nImgTrain):
     # construct string for image
     nImage = im+1
     sImage = str(nImage).zfill(5)
     # open image file
-    img = Image.open(gUrl+lUrl+sTrain+sImage+suffix)
+    imgIm = Image.open(gUrl+lUrl+sTrain+sImage+suffix)
+    imgIm = imgIm.convert("L")
+    # convert to list
+    img1D = imgIm.getdata()
+    # append to train dataset
+    imgTrain.append(img1D)
 
-# handle the image
-img.show()
 
